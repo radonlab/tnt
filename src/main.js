e6c57c24038546676d8bb94cc97c437fe2a96807
skylerlee <skyler.ac.lee@gmail.com>
2025-01-27 23:58:07 +0800

    Support nes loading

--- src/main.js
+++ src/main.js
@@ -4,10 +4,17 @@ function getCanvas() {
   const root = document.getElementById('root');
   const canvas = document.createElement('canvas');
   root.appendChild(canvas);
+  return canvas;
 }
 
 function main() {
-  const nes = new NES();
+  const canvas = getCanvas();
+  const ctx = canvas.getContext('2d');
+  const nes = new NES({
+    onFrame: (frameBuffer) => {
+      console.log(frameBuffer);
+    },
+  });
 
   const nextFrame = () => {
     nes.frame();
